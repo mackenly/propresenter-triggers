39e4bebcef4e34e814fb11686fde5ce57d8243fa
Mackenly Jones <53151058+mackenly@users.noreply.github.com>
2023-09-23 17:58:37 -0400

    Update theme.config.jsx
    Update meta tags

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -10,6 +10,16 @@ export default {
     link: 'https://github.com/mackenly/propresenter-triggers'
   },
   docsRepositoryBase: "https://github.com/mackenly/propresenter-triggers/tree/main/docs",
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="Docs for propresenter-triggers Automation Project" />
+      <meta property="og:title" content="ProPresenter Triggers Docs" />
+      <meta property="og:description" content="Docs for propresenter-triggers Automation Project" />
+      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="theme-color" content="#000000" />
+    </>
+  ),
   footer: {
     text: (
       <span>
